064171574508699d85b914622039839ca99ffbe8
Ondrej Sevcik <Ondrej Sevcik>
2021-08-26 08:32:25 +0200

    map fixed

--- components/homepage/googleMap.js
+++ components/homepage/googleMap.js
@@ -1,4 +1,7 @@
+import { useEffect, useState } from 'react';
+
 const GoogleMap = () => {
+  // <div style={{ position: 'relative', textAlign: 'center', height: '500px', width: '600px', maxWidth: '100%', margin: '0 auto' }}>
   return (
     <div className="text-center">
       <div className="mb-2">
@@ -6,9 +9,9 @@ const GoogleMap = () => {
         <div>Staroveská 567</div>
         <div>739 24 Krmelín</div>
       </div>
-      <div style={{ position: 'relative', textAlign: 'center', height: '500px', width: '600px', margin: '0 auto' }}>
+      <div className="map-frame">
         <iframe
-          width="600"
+          width="100%"
           height="500"
           id="gmap_canvas"
           src="https://maps.google.com/maps?q=Starovesk%C3%A1%20567,%20739%2024%20Krmel%C3%ADn&t=&z=12&ie=UTF8&iwloc=&output=embed"
@@ -16,7 +19,6 @@ const GoogleMap = () => {
           scrolling="no"
           marginHeight="0"
           marginWidth="0"
-          style={{ overflow: 'hidden', background: 'none!important', height: '500px', width: '600px' }}
         ></iframe>
       </div>
     </div>
