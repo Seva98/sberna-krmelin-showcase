5fbf966c17df380f1b7b4f448678c4e7aded9695
Ondrej Sevcik <Ondrej Sevcik>
2021-09-10 14:28:24 +0200

    removed services intro

--- components/services/servicesIntro.js
+++ components/services/servicesIntro.js
@@ -2,7 +2,7 @@ const ServicesIntro = () => {
   return (
     <div className="container text-center py-5">
       <h2 className="h1 pt-2 mb-3 font-weight-bold">Služby</h2>
-      <div className="mb-3 lead">
+      {/* <div className="mb-3 lead">
         <ul style={{ listStyle: 'none' }}>
           <li>Areál disponuje manipulační technikou VZV nosnost 2,5t a vážení na provoze max do 5t.</li>
           <li>Autováha pro nákladní vozidla do 25t na středisku Oprechtice.</li>
@@ -12,7 +12,7 @@ const ServicesIntro = () => {
           <li>Zpracováváme měděné i hliníkové kabely.</li>
           <li> Zajistíme likvidaci autovraků včetně přepravy a dokumentace. (nad 1000 kg)</li>
         </ul>
-      </div>
+      </div> */}
     </div>
   );
 };
